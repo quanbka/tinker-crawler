8b1fa63dd012ea4f132ce29c9b703bb7f687c90c
Le Van Quan <quanbka.cntt@gmail.com>
2022-02-08 14:13:08 +0700

    ok

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-index
+const { readFile } = require('fs');
+
+
+const { XMLParser, XMLBuilder, XMLValidator} = require("fast-xml-parser/src/fxp");
+
+readFile('sitemap_product.xml', { encoding : 'utf8'},  (err, data) => {
+  if (err) throw err;
+  const parser = new XMLParser();
+  let sitemapObj = parser.parse(data);
+  // console.log(sitemapObj.urlset.url.length)
+  sitemapObj.urlset.url.forEach((item, i) => {
+      console.log(item);
+  });
+
+
+});
